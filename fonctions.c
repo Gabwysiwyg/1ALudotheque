35fe3f7a9405616929be834a32c97b39d7811d22
Gabin  Salabert <Gabin@macbook-pro-de-gabin.home>
2016-12-13 22:10:39 +0100

    ftg

--- fonctions.c
+++ fonctions.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-void Menu (char *choix)
+void Menu (int *choix)
 {
 	printf("\n");
 	printf("\t\t\t\t\t*********               |Menu|                *********\n");
 	printf("\n");
-	printf("\t\t\t\t\t||||||||| 1 : Register a new member           |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 2 : Delete a member                 |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 3 : Register a loan                 |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 4 : Register a new theme afternoon  |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 5 : Register member to a theme aft. |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 6 : Je hais l'humanité              |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 7 : Je hais l'humanité              |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 8 : Je hais l'humanité              |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 9 : Exit            |||||||||\n");
+	printf("\t\t\t\t\t||||||||| 1 : Register a new member            |||||||||\n");
+	printf("\t\t\t\t\t||||||||| 2 : Delete a member                  |||||||||\n");
+	printf("\t\t\t\t\t||||||||| 3 : Register a loan                  |||||||||\n");
+	printf("\t\t\t\t\t||||||||| 4 : Register a new theme afternoon   |||||||||\n");
+	printf("\t\t\t\t\t||||||||| 5 : Register member to a theme aft.  |||||||||\n");
+	printf("\t\t\t\t\t||||||||| 6 : Edit the game list               |||||||||\n");
+	printf("\t\t\t\t\t||||||||| 7 : Edit list of afternoon's members |||||||||\n");
+	printf("\t\t\t\t\t||||||||| 8 : Edit borrowers list              |||||||||\n");
+	printf("\t\t\t\t\t||||||||| 9 : Exit                             |||||||||\n");
 	printf("\n");
-	printf("Veuillez faire votre choix\n");
-	scanf("%c%*c", choix);
+	printf("Please, make your choice :");
+	scanf("%d", choix);
 }
---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fonctions.h"
+
+int main (void)
+{
+	int choix;
+
+	while(1){
+        Menu(&choix);
+        while(choix>9)
+        {
+            printf("Wrong choice, it's not a posibility.. Try again.\n");
+            Menu(&choix);
+        }
+
+		switch(choix)
+    	{
+            case 1:
+                printf("\nYou've chosen to register a new member.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 2:
+             	printf("\nYou've chosen to delete a member.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 3:
+                printf("\nYou've chosen to register a loan.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;   
+            case 4:
+                printf("\nYou've chosen to register a new theme afternoon.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 5:
+                printf("\nYou've chosen to register member to a theme aft.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 6:
+                printf("\nYou've chosen to edit the game list.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 7:
+                printf("\nYou've chosen to edit list of afternoon's members.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break; 
+            case 8:
+                printf("\nYou've chosen to edit borrowers list.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 9:
+                printf("\nYou've chosen to exit. \t");
+                printf("Glad you came, see you soon!\n");
+                printf("\n");
+                return 0;  
+        }
+    }
+	return 0;
+}
