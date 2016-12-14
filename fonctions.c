111028e630f9e584b74fb841d1f1c89a29342e90
JunkyPonY <gryfenflash@gmail.com>
2016-12-14 11:28:52 +0100

    add struct & start functions

--- fonctions.c
+++ fonctions.c
@@ -1,22 +1,130 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "fonctions.h"
 
-void Menu (int *choix)
+
+void printMenu (int *choix)
 {
 	printf("\n");
-	printf("\t\t\t\t\t*********               |Menu|                *********\n");
+	printf("*********                |Menu|                *********\n");
 	printf("\n");
-	printf("\t\t\t\t\t||||||||| 1 : Register a new member            |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 2 : Delete a member                  |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 3 : Register a loan                  |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 4 : Register a new theme afternoon   |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 5 : Register member to a theme aft.  |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 6 : Edit the game list               |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 7 : Edit list of afternoon's members |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 8 : Edit borrowers list              |||||||||\n");
-	printf("\t\t\t\t\t||||||||| 9 : Exit                             |||||||||\n");
+	printf("||||||||| 1 : Register a new member            |||||||||\n");
+	printf("||||||||| 2 : Delete a member                  |||||||||\n");
+	printf("||||||||| 3 : Register a loan                  |||||||||\n");
+	printf("||||||||| 4 : Register a new theme afternoon   |||||||||\n");
+	printf("||||||||| 5 : Register member to a theme aft.  |||||||||\n");
+	printf("||||||||| 6 : Edit the game list               |||||||||\n");
+	printf("||||||||| 7 : Edit list of afternoon's members |||||||||\n");
+	printf("||||||||| 8 : Edit borrowers list              |||||||||\n");
+	printf("||||||||| 9 : Exit                             |||||||||\n");
 	printf("\n");
 	printf("Please, make your choice :");
 	scanf("%d", choix);
+}
+
+void Menu()
+{
+	int choix;
+
+	while(1)
+	{
+        Menu(&choix);
+        while(choix>9)
+        {
+            printf("Wrong choice, it's not a posibility.. Try again.\n");
+            printMenu(&choix);
+        }
+
+		switch(choix)
+    	{
+            case 1:
+                printf("\nYou've chosen to register a new member.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 2:
+             	printf("\nYou've chosen to delete a member.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 3:
+                printf("\nYou've chosen to register a loan.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;   
+            case 4:
+                printf("\nYou've chosen to register a new theme afternoon.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 5:
+                printf("\nYou've chosen to register member to a theme aft.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 6:
+                printf("\nYou've chosen to edit the game list.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 7:
+                printf("\nYou've chosen to edit list of afternoon's members.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break; 
+            case 8:
+                printf("\nYou've chosen to edit borrowers list.\n");
+                printf("\n");
+                printf("Do you want to realize another task?\n");
+                break;
+            case 9:
+                printf("\nYou've chosen to exit. \t");
+                printf("Glad you came, see you soon!\n");
+                printf("\n");
+                return 0;  
+        }
+    }
+}
+
+
+Client readClient(FILE *file)
+{
+	Client cli;
+
+	cli.nom[strlen(cli.nom)-1] = '\0'; //nom
+	fgets(cli.nom, 30, file);
+
+	cli.prenom[strlen(cli.prenom)-1] = '\0'; //prenom
+	fgets(cli.prenom, 20, file);
+
+	cli.adresse[strlen(cli.adresse)-1] = '\0'; //adresse
+	fgets(cli.adresse, 50, file);
+
+	cli.ville[strlen(cli.ville)-1] = '\0'; //ville
+	fgets(cli.ville, 20, file);
+
+	fscanf(file, "%d", &(cli.codeP)); //code postal
+
+	cli.paye = 0;
+	cli.nbEmp = 0;
+
+	return cli;
+}
+
+
+void createLiClient(liClient *li)
+{
+	FILE fe;
+	Client cli;
+	elementCli;
+	int nbmax;
+	fe = fopen("client.don", "r");
+	if (fe == NULL)
+	{
+		printf("Can't open file (clients.don)\n");
+		exit(1);
+	}
+
+	fscanf(fe, "%d", &nbmax);
+
+	*li = (liClient)malloc(nbmax*sizeof(elementCli));
+
 }
--- fonctions.h
+++ fonctions.h
@@ -2,4 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-void Menu (int *choix);
+
+typedef enum {false, true} bool;
+
+typedef struct{
+	int jour;
+	int mois;
+	int annee;
+} Date;
+
+typedef struct {
+	char nom[30];
+	char prenom[20];
+	char adresse[50];
+	char ville[20];
+	int codeP;
+	bool paye; //1 si il a payé
+	int nbEmp;
+} Client;
+
+typedef struct {
+	char nom[40];
+	int nbdisp; //nb d'exemplaires dispo
+	int nbtot; //nb d'exemplaires total
+} Jeu;
+
+typedef struct {
+	Client client;
+	Jeu jeu;
+	Date date;
+	bool retard; //1 si en retard
+} Emprunt;
+
+typedef struct {
+	Jeu jeu;
+	Date date;
+	nbPdisp; //nb de places dispo
+	nbPtot; //nb de places totales
+} ApresMidi;
+
+typedef struct {
+	Client client;
+	elementCli *nxt;
+} elementCli;
+typedef elementCli* liClient;
+
+void printMenu(int *choix);
+void Menu ();
+
+void newCli();
+void updateCli(Client client);
+void delCli();
+void newEmprunt(Client client, Jeu jeu);
+void newAfternoon(Jeu jeu, Date date, int nbtot);
+void regForAfternoon(Client client, ApresMidi apMidi);
