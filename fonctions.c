0d832b3e495ae179a601e4e932a3b2e0d09a1b14
Gabin  Salabert <Gabin@pc29.home>
2017-01-07 01:44:59 +0100

    some fix + lowercase

--- fonctions.c
+++ fonctions.c
@@ -206,7 +206,7 @@ void newEmprunt(char *nom, char *prenom, char *game, Jeu **tJeu, int nbj, Client
 
 }
 
-/*Afternoon * newAfternoon(Jeu jeu, Date date, Jeu tJeu[]) //TODO Faire la fonction
+/*Afternoon * newAfternoon(Jeu jeu, Date date, Jeu tJeu[]) //TODO Faire la fonction avec tab pointeurs
 {
 
     afternoon aft;
